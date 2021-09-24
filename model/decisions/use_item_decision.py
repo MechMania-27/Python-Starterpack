3403e8938019fbea3217185a84bb866f4bac6c35
nikwalia <nikash.walia@gmail.com>
2021-09-24 17:42:47 -0500

    Fix use item decision

--- model/decisions/use_item_decision.py
+++ model/decisions/use_item_decision.py
@@ -2,11 +2,11 @@ from model.decisions.action_decision import ActionDecision
 from model.position import Position
 
 class UseItemDecision(ActionDecision):
-    def __init__(self, pos: Position) -> None:
-        self.pos = pos
+    def __init__(self) -> None:
+        super().__init__()
 
     def __str__(self) -> str:
         return "UseItemDecision()"
 
     def engine_str(self) -> str:
-        return f"use_item"
+        return f"use_item "
